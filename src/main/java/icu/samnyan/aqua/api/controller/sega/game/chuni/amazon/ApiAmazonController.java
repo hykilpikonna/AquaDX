d9bb2d61b280347cb9d1c31e7f46f5726d4539dc
samnyan <205-neumphis@users.noreply.dev.s-ul.eu>
2020-02-02 14:27:40 +0800

    [chuni] Add more song info to database

--- src/main/java/icu/samnyan/aqua/api/controller/sega/game/chuni/amazon/ApiAmazonController.java
+++ src/main/java/icu/samnyan/aqua/api/controller/sega/game/chuni/amazon/ApiAmazonController.java
@@ -13,10 +13,7 @@ import icu.samnyan.aqua.sega.chunithm.model.userdata.UserMusicDetail;
 import icu.samnyan.aqua.sega.chunithm.model.userdata.UserPlaylog;
 import icu.samnyan.aqua.sega.chunithm.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
