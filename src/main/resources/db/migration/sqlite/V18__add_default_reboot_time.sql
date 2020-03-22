217ca004897db3090afdec7d479c18929eeb939d
samnyan <205-neumphis@users.noreply.dev.s-ul.eu>
2020-03-23 04:14:14 +0900

    Fix migration for sqlite

--- src/main/resources/db/migration/sqlite/V18__add_default_reboot_time.sql
+++ src/main/resources/db/migration/sqlite/V18__add_default_reboot_time.sql
@@ -1,2 +1,4 @@
-INSERT INTO property (property_key, property_value) VALUE ('reboot_start_time', '2020-01-01 23:59:00.0');
-INSERT INTO property (property_key, property_value) VALUE ('reboot_end_time', '2020-01-01 23:59:00.0');
+INSERT INTO `property` (id, property_key, property_value) VALUES (3, 'reboot_start_time', '2020-01-01 23:59:00.0');
+INSERT INTO `property` (id, property_key, property_value) VALUES (4, 'reboot_end_time', '2020-01-01 23:59:00.0');
+INSERT INTO `property` (id, property_key, property_value) VALUES (5, 'ongeki_music_ng_list', '');
+INSERT INTO `property` (id, property_key, property_value) VALUES (6, 'ongeki_music_recommend_list', '');
