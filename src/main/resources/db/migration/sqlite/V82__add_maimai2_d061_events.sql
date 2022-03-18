3b6a4257acee4c3005548c325bc193a66116b412
Dom Eori <4j6dq2zi8@relay.firefox.com>
2022-03-18 15:18:36 +0900

    [maimai2] Add new event data

---
+++ src/main/resources/db/migration/sqlite/V82__add_maimai2_d061_events.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO `maimai2_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (220391, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',0, false);
+INSERT INTO `maimai2_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (22030321, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',0, true);
