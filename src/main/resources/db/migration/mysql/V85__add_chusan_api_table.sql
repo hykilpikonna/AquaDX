bd97d572a373fa6e1860f97d49468b5bd5791199
Dom Eori <4j6dq2zi8@relay.firefox.com>
2022-03-19 23:36:30 +0900

    [api] Fix MariaDB V85 script

--- src/main/resources/db/migration/mysql/V85__add_chusan_api_table.sql
+++ src/main/resources/db/migration/mysql/V85__add_chusan_api_table.sql
@@ -1,84 +1,59 @@
 CREATE TABLE chusan_music (
-    music_id        INTEGER       NOT NULL,
+    music_id        INTEGER PRIMARY KEY,
     artist_name     VARCHAR (255),
     genre           INTEGER,
     name            VARCHAR (255),
     release_version VARCHAR (255),
-    sort_name       VARCHAR (255),
-    PRIMARY KEY (
-        music_id
-    )
+    sort_name       VARCHAR (255)
+
 );
 
 CREATE TABLE chusan_music_level (
-    id            INTEGER,
+    id            INTEGER auto_increment PRIMARY KEY,
     diff          INTEGER NOT NULL,
     enable        BOOLEAN NOT NULL,
     level         INTEGER NOT NULL,
     level_decimal INTEGER NOT NULL,
-    music_id      INTEGER,
-    PRIMARY KEY (
-        id
-    )
+    music_id      INTEGER
 );
 
 CREATE TABLE chusan_game_character (
-    id               INTEGER,
-    name             VARCHAR NOT NULL,
-    release_tag      VARCHAR NOT NULL,
-    works_name       VARCHAR,
-    illustrator_name VARCHAR,
-    add_images       VARCHAR NOT NULL,
-    PRIMARY KEY (
-        id
-    )
+    id               INTEGER PRIMARY KEY,
+    name             VARCHAR(255) NOT NULL,
+    release_tag      VARCHAR(255) NOT NULL,
+    works_name       VARCHAR(255),
+    illustrator_name VARCHAR(255),
+    add_images       VARCHAR(255) NOT NULL
+
 );
 
 CREATE TABLE chusan_trophy (
-    id   INTEGER,
-    name VARCHAR (255),
-    PRIMARY KEY (
-        id
-    )
+    id   INTEGER PRIMARY KEY,
+    name VARCHAR (255)
 );
 
 CREATE TABLE chusan_nameplate (
-    id   INTEGER,
-    name VARCHAR (255),
-    PRIMARY KEY (
-        id
-    )
+    id   INTEGER PRIMARY KEY,
+    name VARCHAR (255)
 );
 
 CREATE TABLE chusan_voice (
-    id   INTEGER,
-    name VARCHAR (255),
-    PRIMARY KEY (
-        id
-    )
+    id   INTEGER PRIMARY KEY,
+    name VARCHAR (255)
 );
 
 CREATE TABLE chusan_mapicon (
-    id   INTEGER,
-    name VARCHAR (255),
-    PRIMARY KEY (
-        id
-    )
+    id   INTEGER PRIMARY KEY,
+    name VARCHAR (255)
 );
 
 CREATE TABLE chusan_frame (
-    id   INTEGER,
-    name VARCHAR (255),
-    PRIMARY KEY (
-        id
-    )
+    id   INTEGER PRIMARY KEY,
+    name VARCHAR (255)
 );
 
 CREATE TABLE chusan_avatar (
-    id   INTEGER,
+    id   INTEGER PRIMARY KEY,
     name VARCHAR (255),
-    category   INTEGER,
-    PRIMARY KEY (
-        id
-    )
+    category   INTEGER
 );
