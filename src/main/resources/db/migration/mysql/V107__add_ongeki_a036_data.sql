4b96a1d2745c038d7ca34955a9b44b8aa0e5a6af
Dom Eori <4j6dq2zi8@relay.firefox.com>
2022-07-06 10:36:47 +0900

    [ongeki] Add new event and music

---
+++ src/main/resources/db/migration/mysql/V107__add_ongeki_a036_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (788, '40mP feat.シャノ', 100758, 35, 'チュウマイ', '4,0', '7,0', '9,70', '12,40', '0,0', '朝焼けプラットホーム', 'アサヤケフラツトホム');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (867, 't+pazolite feat. しゃま(CV:種﨑 敦美) & みるく(CV:伊藤 あすか)', 100011, 20, 'チュウマイ', '3,0', '8,0', '11,0', '13,50', '0,0', 'とびだせ！TO THE COSMIC!!', 'トヒタセTOTHECOSMIC');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (882, 'ナユタン星人', 100020, 10, 'チュウマイ', '2,0', '7,40', '10,40', '13,20', '0,0', 'コスモポップファンクラブ', 'コスモホツフフアンクラフ');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (885, 'ああああ', 100473, 36, 'チュウマイ', '3,0', '8,40', '11,50', '13,80', '0,0', 'ここからはじまるプロローグ。', 'ココカラハシマルフロロク');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7104, '曲：原田篤（Arte Refact）／歌：星咲 あかり(CV：赤尾 ひかる)', 100631, 40, 'オンゲキ', '4,0', '6,0', '10,0', '12,70', '0,0', '絆はずっとGrowing Up!!! -星咲 あかりソロver.-', 'キスナハスツトGROWINGUPホシサキアカリソロVER');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7105, '曲：原田篤（Arte Refact）／歌：藤沢 柚子(CV：久保田 梨沙)', 100632, 40, 'オンゲキ', '4,0', '6,0', '10,0', '12,70', '0,0', '絆はずっとGrowing Up!!! -藤沢 柚子ソロver.-', 'キスナハスツトGROWINGUPフシサワユスソロVER');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7106, '曲：原田篤（Arte Refact）／歌：三角 葵(CV：春野 杏)', 100633, 40, 'オンゲキ', '4,0', '6,0', '10,0', '12,70', '0,0', '絆はずっとGrowing Up!!! -三角 葵ソロver.-', 'キスナハスツトGROWINGUPミスミアオイソロVER');
+
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1350710301);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1350711301);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1350711601);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1350711801);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1350720301);
