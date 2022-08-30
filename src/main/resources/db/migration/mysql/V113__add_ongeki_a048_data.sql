8f0bc3dc36677a1f2be54b3b3b3c0abecec6d98e
Dom Eori <4j6dq2zi8@relay.firefox.com>
2022-08-30 14:02:54 +0900

    [ongeki] Add new event and music

---
+++ src/main/resources/db/migration/mysql/V113__add_ongeki_a048_data.sql
@@ -0,0 +1,15 @@
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (790, 'Project Grimoire', 100068, 57, 'チュウマイ', '5,0', '9,70', '12,70', '14,60', '0,0', 'Caliburne ～Story of the Legendary sword～', 'CALIBURNESTORYOFTHELEGENDARYSWORD');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (857, 't+pazolite (HARDCORE TANO*C) feat.TANO*C ALL STARS', 100007, 43, 'チュウマイ', '5,0', '9,40', '12,50', '14,30', '0,0', 'ハードコア・シンドローム', 'ハトコアシントロム');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (866, '霜月はるか', 100760, 27, 'チュウマイ', '3,0', '7,0', '10,0', '12,70', '0,0', 'FestivaLight', 'FESTIVALIGHT');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (883, 'R Sound Design feat.向日葵', 100212, 13, 'チュウマイ', '2,0', '7,0', '9,70', '12,40', '0,0', 'Ring', 'RING');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7066, '曲：Haggy Rock & 佐藤厚仁／歌：高瀬 梨緒(CV：久保 ユリカ)', 100031, 40, 'オンゲキ', '4,0', '7,40', '11,60', '13,30', '0,0', '撩乱乙女†無双劇 -高瀬 梨緒ソロver.-', 'リヨウランオトメムソウケキタカセリオソロVER');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7067, '曲：Haggy Rock & 佐藤厚仁／歌：藍原 椿(CV：橋本 ちなみ)', 100045, 40, 'オンゲキ', '4,0', '7,40', '11,60', '13,30', '0,0', '撩乱乙女†無双劇 -藍原 椿ソロver.-', 'リヨウランオトメムソウケキアイハラツハキソロVER');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7068, '曲：Haggy Rock & 佐藤厚仁／歌：結城 莉玖(CV：朝日奈 丸佳)', 100040, 40, 'オンゲキ', '4,0', '7,40', '11,60', '13,30', '0,0', '撩乱乙女†無双劇 -結城 莉玖ソロver.-', 'リヨウランオトメムソウケキユウキリクソロVER');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (8162, 'UNISON SQUARE GARDEN', 100005, 48, 'POPS＆ANIME', '0,0', '0,0', '0,0', '0,0', '13,80', 'シュガーソングとビターステップ', 'シユカソンクトヒタステツフ');
+
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1350910801);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1350910802);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359911324);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359920371);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359920388);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359920390);
