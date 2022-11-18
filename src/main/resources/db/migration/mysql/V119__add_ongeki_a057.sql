5f99f7f8cbc4e01005313b291ec7400ed96b3d5f
Dom Eori <4j6dq2zi8@relay.firefox.com>
2022-11-18 19:07:30 +0900

    [ongeki] Add new event and music

---
+++ src/main/resources/db/migration/mysql/V119__add_ongeki_a057.sql
@@ -0,0 +1,4 @@
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (909, '香椎モイミ', 100208, 2, 'niconico', '2,0', '6,0', '9,40', '12,30', '0,0', 'キャットラビング', 'キヤツトラヒンク');
+
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1351120302);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1351121601);
