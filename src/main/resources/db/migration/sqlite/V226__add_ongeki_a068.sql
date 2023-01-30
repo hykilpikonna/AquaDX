117b116661aa61efa97d3d2e139e759d36f81b6a
Dom Eori <4j6dq2zi8@relay.firefox.com>
2023-01-30 19:32:04 +0900

    [ongeki] Add new event and music

---
+++ src/main/resources/db/migration/sqlite/V226__add_ongeki_a068.sql
@@ -0,0 +1,21 @@
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1351220801);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1351320801);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1351420302);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1351420801);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1351440301);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359911305);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359911610);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359920307);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359920341);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359920379);
+INSERT INTO `ongeki_game_event` (`id`) VALUES (1359920384);
+
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (810, 'kanone feat. せんざい', 100062, 54, 'チュウマイ', '5,0', '8,40', '11,70', '14,50', '0,0', '花と、雪と、ドラムンベース。', 'ハナトユキトトラムンヘス');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (890, 'ETIA.', 100444, 51, 'チュウマイ', '5,0', '9,70', '13,30', '14,80', '0,0', 'Hainuwele', 'HAINUWELE');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (908, 'P丸様。', 100082, 6, 'POPS＆ANIME', '3,0', '7,0', '9,70', '12,60', '0,0', 'シル・ヴ・プレジデント', 'シルウフレシテント');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (912, 'KOTOKO', 100262, 5, 'POPS＆ANIME', '2,0', '6,0', '9,40', '12,40', '0,0', '恋ひ恋ふ縁', 'コヒコウエニシ');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (913, 'ツミキ', 100043, 11, 'niconico', '3,0', '7,0', '10,0', '12,70', '0,0', 'フォニイ', 'フオニイ');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7081, '曲：高木龍一(Dream Monster)／歌：三角 葵(CV：春野 杏)', 100264, 40, 'オンゲキ', '4,0', '7,70', '9,70', '13,30', '0,0', 'YAMINABE☆PANIC ～ご馳走詰め込みフルコース～ -三角 葵ソロver.-', 'YAMINABEPANICコチソウツメコメフルコスミスミアオイソロVER');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7082, '曲：高木龍一(Dream Monster)／歌：桜井 春菜(CV：近藤 玲奈)', 100476, 40, 'オンゲキ', '4,0', '7,70', '9,70', '13,30', '0,0', 'YAMINABE☆PANIC ～ご馳走詰め込みフルコース～ -桜井 春菜ソロver.-', 'YAMINABEPANICコチソウツメコメフルコスサクライハルナソロVER');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (7083, '曲：高木龍一(Dream Monster)／歌：珠洲島 有栖(CV：長縄 まりあ)', 100446, 40, 'オンゲキ', '4,0', '7,70', '9,70', '13,30', '0,0', 'YAMINABE☆PANIC ～ご馳走詰め込みフルコース～ -珠洲島 有栖ソロver.-', 'YAMINABEPANICコチソウツメコメフルコスススシマアリスソロVER');
+INSERT INTO ongeki_game_music (id, artist_name, boss_card_id, boss_level, genre, level0, level1, level2, level3, level4, name, sort_name) VALUES (8167, 'P丸様。', 100082, 30, 'POPS＆ANIME', '0,0', '0,0', '0,0', '0,0', '13,60', 'シル・ヴ・プレジデント', 'シルウフレシテント');
