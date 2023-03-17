938ab164b412330adb31d7240d595ccbdf378c94
Dom Eori <4j6dq2zi8@relay.firefox.com>
2023-03-17 15:42:59 +0900

    [chusan] Add Sun base data

---
+++ src/main/resources/db/migration/sqlite/V231__add_chusan_sun_a000.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO `chusan_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (12009, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',2, 1);
+INSERT INTO `chusan_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (12010, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',2, 1);
+INSERT INTO `chusan_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (12011, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',2, 1);
+INSERT INTO `chusan_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (12012, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',2, 1);
+INSERT INTO `chusan_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (12013, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',2, 1);
+INSERT INTO `chusan_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (12014, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',2, 1);
+INSERT INTO `chusan_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (12015, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',2, 1);
+INSERT INTO `chusan_game_event` (`id`, `end_date`, `start_date`, `type`, `enable`) VALUES (12016, '2029-01-01 00:00:00.000000', '2019-01-01 00:00:00.000000',2, 1);
+INSERT INTO chusan_frame (id, name) VALUES (2, 'SUN');
