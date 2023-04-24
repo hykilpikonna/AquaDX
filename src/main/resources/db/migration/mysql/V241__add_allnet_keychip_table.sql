ae698159effdd58128b04dba8fa85e86d7c01a2e
Fleming Karlzett <schdsrlin@gmail.com>
2023-04-24 12:22:41 +0000

    [allnet] Add keychip verify on poweron request

---
+++ src/main/resources/db/migration/mysql/V241__add_allnet_keychip_table.sql
@@ -0,0 +1,8 @@
+create table allnet_keychips
+(
+    id         int(20)      primary key AUTOINCREMENT not null,
+    keychip_id varchar(255) not null
+)
+    ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    collate = utf8mb4_unicode_ci;
---
+++ src/main/resources/db/migration/sqlite/V241__add_allnet_keychip_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE allnet_keychips
+(
+    id        INTEGER NOT NULL,
+    keychip_id     VARCHAR(255),
+    PRIMARY KEY (
+         id
+    )
+);
