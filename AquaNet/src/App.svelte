071491b4591918e16beb8b41188a166b6104ec20
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-08 02:51:31 -0500

    [M] Rename

---
+++ AquaNet/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Router, Route } from "svelte-routing";
+  import Home from "./pages/Home.svelte";
+
+  import MaimaiRating from "./pages/MaimaiRating.svelte";
+
+  export let url = "";
+</script>
+
+<Router {url}>
+  <div>
+    <Route path="/"><Home /></Route>
+    <Route path="/mai/rating"><MaimaiRating /></Route>
+  </div>
+</Router>
---
+++ AquaNet/src/libs/config.ts
@@ -0,0 +1,5 @@
+
+const aqua_host = "https://aquadx.hydev.org"
+const data_host = "https://aqua-data.hydev.org"
+
+export { aqua_host, data_host }
