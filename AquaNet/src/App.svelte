ad3bb89dc9c35f37bdd538a8c4b05fc932a6732f
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-10 00:40:56 -0500

    [+] Home site

--- AquaNet/src/App.svelte
+++ AquaNet/src/App.svelte
@@ -3,6 +3,7 @@
   import Home from "./pages/Home.svelte";
 
   import MaimaiRating from "./pages/MaimaiRating.svelte";
+  import UserHome from "./pages/UserHome.svelte";
 
   export let url = "";
 </script>
@@ -10,6 +11,7 @@
 <Router {url}>
   <div>
     <Route path="/"><Home /></Route>
+    <Route path="/u/:userId" component={UserHome}></Route>
     <Route path="/u/:userId/mai/rating" component={MaimaiRating}></Route>
   </div>
 </Router>
--- AquaNet/src/pages/Home.svelte
+++ AquaNet/src/pages/Home.svelte
@@ -1,3 +1,26 @@
-<main>
-  owo
+<main id="home">
+  <h1>AquaNet</h1>
+  <div>
+    <button>Login</button>
+    <button>Sign Up</button>
+  </div>
 </main>
+
+<style lang="sass">
+  @import "../vars"
+
+  #home
+    color: $c-main
+
+    > h1
+      font-family: Quicksand, $font
+      user-select: none
+
+      // Gap between text characters
+      letter-spacing: 0.2em
+    
+    > div
+      display: flex
+      gap: 8px
+    
+</style>
