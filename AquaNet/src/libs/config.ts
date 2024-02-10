bb3902730be44930695f03f7a349ac5449d3ddc1
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-09 21:55:08 -0500

    [U] Update url

--- AquaNet/src/libs/config.ts
+++ AquaNet/src/libs/config.ts
@@ -1,5 +1,5 @@
 
-const aqua_host = 'https://aquanet.example.com/Maimai2Servlet'
+const aqua_host = 'https://aquanet.example.com/aqua'
 const data_host = 'https://aquanet.example.com'
 
 export { aqua_host, data_host }
