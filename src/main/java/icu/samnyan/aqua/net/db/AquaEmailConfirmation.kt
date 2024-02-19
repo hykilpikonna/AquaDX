5715fa97f742351e1b2e89a5afd139caac84d443
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-19 06:58:51 -0500

    [+] Email confirmation table

---
+++ src/main/java/icu/samnyan/aqua/net/db/AquaEmailConfirmation.kt
@@ -0,0 +1,32 @@
+package icu.samnyan.aqua.net.db
+
+import jakarta.persistence.*
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import java.io.Serializable
+import java.time.Instant
+
+@Entity
+@Table(name = "aqua_net_email_confirmation")
+class EmailConfirmation(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(nullable = false)
+    var token: String = "",
+
+    // Token creation time
+    @Column(nullable = false)
+    var createdAt: Instant = Instant.now(),
+
+    // Linking to the AquaNetUser
+    @OneToOne
+    @JoinColumn(name = "auId", referencedColumnName = "auId")
+    var aquaNetUser: AquaNetUser = AquaNetUser()
+) : Serializable
+
+@Repository
+interface EmailConfirmationRepo : JpaRepository<EmailConfirmation, Long> {
+    fun findByToken(token: String): EmailConfirmation?
+}
