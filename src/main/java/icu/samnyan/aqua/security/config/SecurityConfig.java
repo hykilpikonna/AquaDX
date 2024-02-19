94a32348748ba13645a8093cad1b6b775ae1b872
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-19 01:37:50 -0500

    [O] CORS allow all

--- src/main/java/icu/samnyan/aqua/security/config/SecurityConfig.java
+++ src/main/java/icu/samnyan/aqua/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package icu.samnyan.aqua.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -33,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.headers(headers -> headers.disable())
-                .cors(cors -> cors.disable())
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
 
