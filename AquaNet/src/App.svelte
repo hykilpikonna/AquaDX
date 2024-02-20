133140bf71d3bdc1119ee231b42db04a9ddb9b7d
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-20 18:46:15 -0500

    [O] Rename home to welcome

--- AquaNet/src/App.svelte
+++ AquaNet/src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { Router, Route } from "svelte-routing";
-  import Home from "./pages/Home.svelte";
+  import Welcome from "./pages/Welcome.svelte";
   import MaimaiRating from "./pages/MaimaiRating.svelte";
   import UserHome from "./pages/UserHome.svelte";
   import Icon from '@iconify/svelte';
@@ -16,9 +16,9 @@
 </nav>
 
 <Router {url}>
-  <Route path="/"><Home /></Route>
-  <Route path="/u/:userId" component={UserHome}></Route>
-  <Route path="/u/:userId/mai/rating" component={MaimaiRating}></Route>
+  <Route path="/" component={Welcome} />
+  <Route path="/u/:userId" component={UserHome} />
+  <Route path="/u/:userId/mai/rating" component={MaimaiRating} />
 </Router>
 
 <style lang="sass">
