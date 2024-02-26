7cef8f24dbbde0ffb4219f9392f91c1622d5c83a
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-26 11:59:53 -0500

    [-] Remove mysql

--- src/main/resources/db/migration/mysql/V255__add_maimai_140_ext_field.sql
+++
@@ -1,8 +0,0 @@
--- maimai2_user_playlog
-ALTER TABLE maimai2_user_playlog ADD COLUMN ext_bool1 BOOLEAN;
-ALTER TABLE maimai2_user_playlog ADD COLUMN ext_num4 INTEGER;
-UPDATE maimai2_user_playlog SET ext_num4=0;
-
--- maimai2_user_detail
-ALTER TABLE maimai2_user_detail ADD COLUMN current_play_count INTEGER;
-ALTER TABLE maimai2_user_detail ADD COLUMN rename_credit INTEGER;
