256f08396f501671ae3bedef3ceb8aa0ceeaf0b8
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-28 17:55:52 -0500

    [+] Setup instrcutions page

--- AquaNet/src/pages/Home.svelte
+++ AquaNet/src/pages/Home.svelte
@@ -1,9 +1,11 @@
 <script>
   import LinkCard from "./Home/LinkCard.svelte";
+  import SetupInstructions from "./Home/SetupInstructions.svelte";
 </script>
 
 <main class="content">
   <h2>Welcome to AquaDX!</h2>
 
   <LinkCard/>
+  <SetupInstructions/>
 </main>
---
+++ AquaNet/src/pages/Home/SetupInstructions.svelte
@@ -0,0 +1,14 @@
+
+<div class="setup-instructions">
+  <h2>Connection Setup</h2>
+  <p>
+    Welcome! If you own an arcade cabinet or game setup,
+    please follow the instructions below to set up the connection with AquaDX.
+  </p>
+  <blockquote>
+    We assume that you already have the required files and can run the game (e.g. ROM and segatools)
+    that come with the cabinet or game setup.
+    If not, please contact the seller of your device for the required files, as we will not provide them for copyright reasons.
+  </blockquote>
+
+</div>
