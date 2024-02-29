856bcf1647195fe7b1852bdfee0a2ed48c8caed5
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-02-29 00:25:06 -0500

    [+] Aqua connection host in config

--- AquaNet/src/libs/config.ts
+++ AquaNet/src/libs/config.ts
@@ -2,6 +2,9 @@
 const AQUA_HOST = 'https://aquanet.example.com/aqua'
 const DATA_HOST = 'https://aquanet.example.com'
 
+// This will be displayed for users to connect from the client
+const AQUA_CONNECTION = 'aqua.example.com'
+
 const TURNSTILE_SITE_KEY = '0x4AAAAAAASGA2KQEIelo9P9'
 
 export { AQUA_HOST, DATA_HOST, TURNSTILE_SITE_KEY }
