a5578335d372e21e23916f0892a33d3d83565064
Galexion <Voltaire577@gmail.com>
2024-02-29 22:50:58 -0500

    AquaNet: Append to make Error Message h2 not use margin-top

--- AquaNet/src/ErrorMessage.svelte
+++ AquaNet/src/ErrorMessage.svelte
@@ -4,7 +4,7 @@
     export let ifError = `No Error was Passed through. Quick, check the Devtools and create a Ticket!`;
 </script>
 <blockquote>
-    <h2 style="color:red;">Something's wrong, Please Try again Later.</h2>
+    <h2 style="color:red; margin-top:0px;">Something's wrong, Please Try again Later.</h2>
     <p>Need to know why? Check Below.</p>
     <code>
       {ifError}
