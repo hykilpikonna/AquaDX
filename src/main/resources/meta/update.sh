5913d5b58591b8fa1616b022d2b8f5adf431a705
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-03-15 01:13:18 -0400

    [F] Fix script

--- src/main/resources/meta/update.sh
+++ src/main/resources/meta/update.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-curl "https://aquadx.net/d/ongeki/00/all-music.json" -o "ongeki/music.json"
-curl "https://aquadx.net/d/mai2/00/all-music.json" -o "mai2/music.json"
-curl "https://aquadx.net/d/chu3/00/all-music.json" -o "chu3/music.json"
+
+# Current script dir
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+mkdir -p "$DIR/ongeki" "$DIR/mai2" "$DIR/chu3"
+
+curl "https://aquadx.net/d/ongeki/00/all-music.json" -o "$DIR/ongeki/music.json"
+curl "https://aquadx.net/d/mai2/00/all-music.json" -o "$DIR/mai2/music.json"
+curl "https://aquadx.net/d/chu3/00/all-music.json" -o "$DIR/chu3/music.json"
