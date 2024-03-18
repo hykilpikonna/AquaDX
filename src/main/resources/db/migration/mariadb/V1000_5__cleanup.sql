e0c79984482546671d5e2b3ee107fe5f6120dd8a
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-03-18 01:31:39 -0400

    [+] DB cleanup

---
+++ src/main/resources/db/migration/mariadb/V1000_5__cleanup.sql
@@ -0,0 +1,11 @@
+ALTER TABLE bak_maimai2_user_rate
+    DROP FOREIGN KEY FKfaewgvanchzwo8um;
+
+DROP TABLE bak_diva_pv_entry;
+
+DROP TABLE bak_maimai2_user_rate;
+
+DROP TABLE hibernate_sequence;
+
+ALTER TABLE chusan_user_data
+    DROP COLUMN rank_up_challenge_results;
