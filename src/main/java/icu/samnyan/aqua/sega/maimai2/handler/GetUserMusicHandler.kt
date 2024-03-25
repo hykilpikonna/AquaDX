30f740a430502adb5e62d138aac9db7a10167bc6
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-03-25 13:56:49 -0400

    [-] Remove old code

--- src/main/java/icu/samnyan/aqua/sega/maimai2/handler/GetUserMusicHandler.kt
+++ src/main/java/icu/samnyan/aqua/sega/maimai2/handler/GetUserMusicHandler.kt
@@ -14,23 +14,6 @@ class GetUserMusicHandler(
 ) : BaseHandler {
     override fun handle(request: Map<String, Any>): Any {
         val userId = (request["userId"] as Number).toLong()
-//        val nextIndexVal = (request["nextIndex"] as Number).toInt()
-//        val maxCount = (request["maxCount"] as Number).toInt()
-//
-//        val pageNum = nextIndexVal / maxCount
-//
-//        val dbPage = userMusicDetailRepository.findByUser_Card_ExtId(userId, PageRequest.of(pageNum, maxCount))
-//
-//        val currentIndex = (maxCount * pageNum + dbPage.numberOfElements).toLong()
-//
-//        val resultMap = mutableMapOf(
-//            "userId" to userId,
-//            "nextIndex" to if (dbPage.numberOfElements < maxCount) 0 else currentIndex,
-//            "userMusicList" to listOf(mapOf("userMusicDetailList" to dbPage.content))
-//        )
-//
-//        val json = mapper.write(resultMap)
-//        logger.info("Response: $json")
 
         val db = repos.userMusicDetail.findByUser_Card_ExtId(userId)
 
