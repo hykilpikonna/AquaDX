f97cb4a1bba89d26d5e0c85c8174a9a89dbca987
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-03-28 19:02:21 -0400

    [F] Fix integer list converter behavior on empty lists

--- src/main/java/icu/samnyan/aqua/sega/general/IntegerListConverter.kt
+++ src/main/java/icu/samnyan/aqua/sega/general/IntegerListConverter.kt
@@ -6,5 +6,6 @@ import jakarta.persistence.Converter
 @Converter
 class IntegerListConverter : AttributeConverter<List<Int>, String> {
     override fun convertToDatabaseColumn(lst: List<Int>?) = lst?.joinToString(";") ?: ""
-    override fun convertToEntityAttribute(str: String?) = str?.split(';')?.map { it.toInt() }?.toMutableList() ?: mutableListOf()
+    override fun convertToEntityAttribute(str: String?) = if (str.isNullOrBlank()) mutableListOf() else
+        str.split(';').map { it.toInt() }.toMutableList()
 }
