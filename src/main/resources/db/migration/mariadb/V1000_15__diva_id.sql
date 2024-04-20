e001533f33d31f66b2305f1c2b27a366036918c2
Azalea <22280294+hykilpikonna@users.noreply.github.com>
2024-04-20 14:20:09 +0900

    [F] Fix diva migration sql

--- src/main/resources/db/migration/mariadb/V1000_15__diva_id.sql
+++ src/main/resources/db/migration/mariadb/V1000_15__diva_id.sql
@@ -1,4 +1,4 @@
-ALTER TABLE main.diva_player_profile MODIFY COLUMN pd_id BIGINT NOT NULL;
-ALTER TABLE main.diva_player_profile MODIFY COLUMN rival_pd_id BIGINT NOT NULL;
-ALTER TABLE main.diva_player_screen_shot MODIFY COLUMN pd_id BIGINT NOT NULL;
-ALTER TABLE main.diva_player_screen_shot MODIFY COLUMN pv_id BIGINT NOT NULL;
+ALTER TABLE diva_player_profile MODIFY COLUMN pd_id BIGINT NOT NULL;
+ALTER TABLE diva_player_profile MODIFY COLUMN rival_pd_id BIGINT NOT NULL;
+ALTER TABLE diva_player_screen_shot MODIFY COLUMN pd_id BIGINT NOT NULL;
+ALTER TABLE diva_player_screen_shot MODIFY COLUMN pv_id BIGINT NOT NULL;
