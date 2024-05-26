f6d55fec3557a85ede02057f7efb7ffa50bc630c
Clansty <i@gao4.pw>
2024-05-26 13:36:56 +0800

    [+] Skip "Discovered new area"

--- AquaMai/UX/SkipEventInfo.cs
+++ AquaMai/UX/SkipEventInfo.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using HarmonyLib;
+using Process;
 using Process.Information;
 
 namespace AquaMai.UX
@@ -7,9 +9,16 @@ namespace AquaMai.UX
     {
         [HarmonyPostfix]
         [HarmonyPatch(typeof(InformationProcess), "OnStart")]
-        public static void InformationProcessPreStart(ref uint ____state)
+        public static void InformationProcessPostStart(ref uint ____state)
         {
             ____state = 3;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(RegionalSelectProcess), "OnStart")]
+        public static void RegionalSelectProcessPreStart(ref Queue<int>[] ____discoverList)
+        {
+            ____discoverList = new Queue<int>[] { new Queue<int>(), new Queue<int>() };
         }
     }
 }
