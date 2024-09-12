81c1e6e88777ea5cd12a37a08be8cfe79967b69a
Clansty <i@gao4.pw>
2024-09-12 21:56:23 +0800

    [+] Increase version code

--- AquaMai/Main.cs
+++ AquaMai/Main.cs
@@ -13,7 +13,7 @@ namespace AquaMai
         public const string Description = "Mod for Sinmai";
         public const string Author = "Aza";
         public const string Company = null;
-        public const string Version = "1.0.4";
+        public const string Version = "1.0.5";
         public const string DownloadLink = null;
     }
 
