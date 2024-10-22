1bd04912ab70374dc4caeae0fcb17825d75e2d2e
Fukim <1394985468@qq.com>
2024-10-22 20:58:29 +0800

    修复在SDGB下直接SetNextProcess导致摄像头未能正确关闭而引发的帧率波动

--- AquaMai/UX/SinglePlayer.cs
+++ AquaMai/UX/SinglePlayer.cs
@@ -1,5 +1,8 @@
+using System.Reflection;
+using System;
 using HarmonyLib;
 using MAI2.Util;
+using MAI2System;
 using Manager;
 using MelonLoader;
 using Monitor.Common;
@@ -14,6 +17,8 @@ namespace AquaMai.UX
     // Note: this is not my original work. I simply interpreted the code and rewrote it as a mod.
     public class SinglePlayer
     {
+        public static bool IsSDGB { get; set; } = SDGB_Check();
+
         [HarmonyPrefix]
         [HarmonyPatch(typeof(Main.GameMain), "LateInitialize", new Type[] { typeof(MonoBehaviour), typeof(Transform), typeof(Transform) })]
         public static void LateInitialize(MonoBehaviour gameMainObject, ref Transform left, ref Transform right)
@@ -42,7 +47,10 @@ namespace AquaMai.UX
             __instance.Process.CreateDownloadProcess();
             __instance.ProcessManager.SendMessage(new Message(ProcessType.CommonProcess, 30001));
             __instance.ProcessManager.SendMessage(new Message(ProcessType.CommonProcess, 40000, 0, OperationInformationController.InformationType.Hide));
-            __instance.Process.SetNextProcess();
+            SingletonStateMachine<AmManager, AmManager.EState>.Instance.AimeReader.EnableRead(false);
+            SingletonStateMachine<AmManager, AmManager.EState>.Instance.AimeReader.BeginPlay();
+            // SDGB 在扫码状态下会降低帧率以适应摄像头，直接SetNextProcess会导致摄像头未能正确关闭
+            if (SinglePlayer.IsSDGB != true){ __instance.Process.SetNextProcess();}
         }
 
         // To prevent the "長押受付終了" overlay from appearing
@@ -50,7 +58,34 @@ namespace AquaMai.UX
         [HarmonyPatch(typeof(WaitPartner), "Open")]
         public static bool WaitPartnerPreOpen()
         {
+            if (SinglePlayer.IsSDGB == true) { return true; }
             return false;
+        }
+
+        public static bool SDGB_Check() {
+
+            String gameID = "";
+            // 检测游戏版本并判断是否为 SDGB , Thanks to Sinmai-Assist !!!
+            try
+            {
+                gameID = (string)typeof(ConstParameter).GetField("GameIDStr",
+                BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy).GetValue(null);
+                
+            }
+            catch (Exception e)
+            {
+                MelonLogger.Error("[SinglePlayer] Failed to get GameIDStr");
+                MelonLogger.Error(e);
+            }
+
+            if (gameID.Equals("SDGB")) {
+                MelonLogger.Warning("[SinglePlayer] Inject Game is SDGB!");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
